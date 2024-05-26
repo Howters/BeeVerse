6fa9c64e98444a2edfd6505b551cc51c37d822c8
Hans Khomulia <hans.khomulia@binus.edu>
2024-05-27 00:34:48 +0700

    fix vite manifest error v.2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig(
-{
+export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/scss/style.scss',
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
