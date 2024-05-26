e5bc127310055fb46dd515ebda87279207d2cc35
Hans Khomulia <Hanskho321@gmail.com>
2024-05-27 02:37:01 +0700

    changed css homepage path to absolute

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,7 @@
     <title>Beeverse</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     {{-- <link rel="stylesheet" href="{{asset('css/homepage.css')}}"> --}}
-    <link rel="stylesheet" href="css/homepage.css">
+    <link rel="stylesheet" href="/css/homepage.css">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
