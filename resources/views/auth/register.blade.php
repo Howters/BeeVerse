e50d0638287f3d947de852aab8dd614ec2f2c0c8
Hans Khomulia <hans.khomulia@binus.edu>
2024-05-27 01:24:48 +0700

    fix image file name in register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,17 +8,15 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Login</title>
+    <title>Register</title>
 </head>
 <body>
     <x-navbar />
   <div class="wrapper">
     <div class="container main">
         <div class="row">
-            <div class="col-md-6 side-image" style="background-image: url('{{asset('assets/Binus-image-1.jpg')}}')">
-
+            <div class="col-md-6 side-image" style="background-image: url('{{asset('assets/Binus-Image-1.jpg')}}')">
             </div>
-
             <div class="col-md-6 right">
                 <form id="registrationForm" method="POST" action="{{ route('register') }}">
                     @csrf
