76ceb175b99ffc21d753ff68953203c3f7cba7e1
Hans Khomulia <Hanskho321@gmail.com>
2024-06-07 22:48:14 +0700

    bugfix: fix css bug in homepage for deployment

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,7 @@
     <title>Beeverse</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     {{-- <link rel="stylesheet" href="{{asset('css/homepage.css')}}"> --}}
-    <link rel="stylesheet" href="/css/homepage.css">
+    <link rel="stylesheet" href="css/homepage.css">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
